80bdcdf2059bfeff2c9189a6a7151603f78bdeb3
CubeyTheCube <72284516+CubeyTheCube@users.noreply.github.com>
2020-12-15 12:05:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ bot.on('message', msg => {
 				this.arcTo(x, y, x + w, y, r);
 				this.closePath();
 				return this.fill();
-			} // draws tiles
+			} 
 			function arraysEqual(a, b) {
 				if (a instanceof Array && b instanceof Array) {
 					if (a.length != b.length)
@@ -41,10 +41,10 @@ bot.on('message', msg => {
 					return true;
 				} else {
 					return a == b;
-				} // code I copied from stack overflow
+				} 
 			}
-			const color = ["eee4da", "ede0c8", "f2b179", "f59563", "f67c5f", "f65e3b", "edcf72", "edcc61", "edc850", "edc53f", "edc22e", "3c3a32"]; // tile colors
-			function tile(x, y, num) { // renders tiles
+			const color = ["eee4da", "ede0c8", "f2b179", "f59563", "f67c5f", "f65e3b", "edcf72", "edcc61", "edc850", "edc53f", "edc22e", "3c3a32"]; 
+			function tile(x, y, num) { 
 				if (num == 0) {
 					ctx.fillStyle = "#CDC1B4";
 					ctx.roundRect(x, y, 97, 97, 4);
@@ -58,7 +58,7 @@ bot.on('message', msg => {
 				}
 			}
 
-			function update() { // tick
+			function update() { 
 				ctx.clearRect(0, 0, 500, 550);
 				ctx.fillStyle = '#bbada0';
 				ctx.roundRect(0, 0, 444, 444, 6);
@@ -78,8 +78,8 @@ bot.on('message', msg => {
 				[0, 0, 0, 0],
 				[0, 0, 0, 0],
 				[0, 0, 0, 0]
-			]; // starting board state
-			function addRandomTile() { // adds random tile
+			]; 
+			function addRandomTile() { 
 				var av = [];
 				for (var i = 0; i < 4; i++) {
 					for (var j = 0; j < 4; j++) {
@@ -91,27 +91,27 @@ bot.on('message', msg => {
 				var coord = av[Math.floor(Math.random() * av.length)];
 				board[coord[0]][coord[1]] = Math.random() < 0.9 ? 2 : 4;
 			}
-			const vec = [ // vectors representing directions for moving tiles
+			const vec = [ 
 				[-1, 0],
 				[0, 1],
 				[1, 0],
 				[0, -1]
 			];
 
-			function moveTile(k, l, dir) { // moves tiles
-				var travX = dir == 2 ? [3, 2, 1, 0] : [0, 1, 2, 3]; // makes the tiles merge in the right order
+			function moveTile(k, l, dir) { 
+				var travX = dir == 2 ? [3, 2, 1, 0] : [0, 1, 2, 3]; 
 				var travY = dir == 1 ? [3, 2, 1, 0] : [0, 1, 2, 3];
 				x = travX[k];
 				y = travY[l];
-				var dx = vec[dir][0]; // direction of tile movement
+				var dx = vec[dir][0]; 
 				var dy = vec[dir][1];
-				if ((x + dx + 1) % 5 != 0 && (y + dy + 1) % 5 != 0) { // make sure tile is not going off the board
-					while ((x + dx + 1) % 5 != 0 && (y + dy + 1) % 5 != 0) { // move until the tile is on the edge
-						if (board[x + dx][y + dy] != 0) { // if it hits a tile stop
+				if ((x + dx + 1) % 5 != 0 && (y + dy + 1) % 5 != 0) { 
+					while ((x + dx + 1) % 5 != 0 && (y + dy + 1) % 5 != 0) { 
+						if (board[x + dx][y + dy] != 0) { 
 							return;
 						}
-						board[x + dx][y + dy] = board[x + dx - vec[dir][0]][y + dy - vec[dir][1]]; // replace the next tile witht the tile before it
-						board[x + dx - vec[dir][0]][y + dy - vec[dir][1]] = 0; // cancels the previous tile 
+						board[x + dx][y + dy] = board[x + dx - vec[dir][0]][y + dy - vec[dir][1]]; 
+						board[x + dx - vec[dir][0]][y + dy - vec[dir][1]] = 0; 
 						dx += vec[dir][0];
 						dy += vec[dir][1];
 					}
@@ -126,14 +126,14 @@ bot.on('message', msg => {
 				var dx = vec[dir][0];
 				var dy = vec[dir][1];
 				if ((x + dx + 1) % 5 != 0 && (y + dy + 1) % 5 != 0) {
-					if (board[x + dx][y + dy] == board[x][y] && mergedTiles[x][y] == 0) { // if the two tiles are the same number and that tile has not been merged yet, MERGE
-						board[x + dx][y + dy] += board[x + dx][y + dy]; // double the tile value
+					if (board[x + dx][y + dy] == board[x][y] && mergedTiles[x][y] == 0) { 
+						board[x + dx][y + dy] += board[x + dx][y + dy]; 
 						score += board[x + dx][y + dy];
-						board[x][y] = 0; // cancel the first tile
-						mergedTiles[x + dx][y + dy] = 1; // add it to the list of merged tiles
+						board[x][y] = 0; 
+						mergedTiles[x + dx][y + dy] = 1; 
 					} else {
 						moveTile(k, l, dir);
-					} // if the tile did not merge then move it
+					} 
 				}
 			}
 			var mergedTiles;
@@ -142,7 +142,7 @@ bot.on('message', msg => {
 				var travX = dir == 2 ? [3, 2, 1, 0] : [0, 1, 2, 3];
 				var travY = dir == 1 ? [3, 2, 1, 0] : [0, 1, 2, 3];
 				var len = board.length,
-					tmp = new Array(len); // duplicate the board state 
+					tmp = new Array(len); 
 				for (var i = 0; i < len; ++i) {
 					tmp[i] = board[i].slice(0);
 				}
@@ -151,24 +151,24 @@ bot.on('message', msg => {
 					[0, 0, 0, 0],
 					[0, 0, 0, 0],
 					[0, 0, 0, 0]
-				]; // setup the list of merged tiles
+				]; 
 				for (var m = 0; m < 2; m++) {
-					for (var k = 0; k < 4; k++) { // loop through all tiles
+					for (var k = 0; k < 4; k++) { 
 						for (var l = 0; l < 4; l++) {
-							mergeTile(k, l, dir); // moves/merges the tiles
+							mergeTile(k, l, dir); 
 						}
 					}
 				}
-				if ((board[0].includes(0) || board[1].includes(0) || board[2].includes(0) || board[3].includes(0)) && !arraysEqual(board, tmp)) { // if there is space to move and the board state changed, then add a random tile
+				if ((board[0].includes(0) || board[1].includes(0) || board[2].includes(0) || board[3].includes(0)) && !arraysEqual(board, tmp)) { 
 					addRandomTile();
 					update();
 				}
 			}
 			score = 0;
-			addRandomTile(); // add the two initial tiles
+			addRandomTile(); 
 			addRandomTile();
 			update();
-			const keys = ['ArrowLeft', 'ArrowDown', 'ArrowRight', 'ArrowUp', 'a', 's', 'd', 'w', 'h', 'j', 'k', 'l']; // list of keys
+			const keys = ['ArrowLeft', 'ArrowDown', 'ArrowRight', 'ArrowUp', 'a', 's', 'd', 'w', 'h', 'j', 'k', 'l']; 
 			const attachment = new Discord.MessageAttachment(c.toBuffer(), 'welcome-image.png');
 			bot.channels.cache.get(process.env.IMAGE_CHANNEL).send(attachment).then(message2 => {
 				const game = new Discord.MessageEmbed()
